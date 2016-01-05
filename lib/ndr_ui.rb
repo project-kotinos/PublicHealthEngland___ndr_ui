27343997e3e856dd82f8af36ad3c66e1922f0d9c
Tim Gentry <tpgentry@me.com>
2016-01-05 08:42:22 +0000

    # Added .rubocop.yml file

--- lib/ndr_ui.rb
+++ lib/ndr_ui.rb
@@ -1,5 +1,6 @@
 require 'ndr_ui/version'
 require 'ndr_ui/engine'
 
+# Any NdrUi configuration or convenience methods should go here
 module NdrUi
 end
--- lib/ndr_ui/engine.rb
+++ lib/ndr_ui/engine.rb
@@ -1,4 +1,5 @@
 module NdrUi
+  # This is where we define the base class for the engine
   class Engine < ::Rails::Engine
   end
 end
