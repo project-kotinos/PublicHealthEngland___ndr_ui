fa1b0ea70f3012aadbb838bc28dee5fd75b40d01
Tim Gentry <tpgentry@me.com>
2016-03-09 00:49:01 +0000

    # Initial commit of our own scaffolded form view, using a horizontal bootstrap form using control_groups

--- lib/ndr_ui/engine.rb
+++ lib/ndr_ui/engine.rb
@@ -8,6 +8,15 @@ module NdrUi
 
     config.assets.paths << File.expand_path('../../../vendor/assets', __FILE__)
 
+    # We configure the generator of the host app
+    config.app_generators do |g|
+      # Prepend our scaffold template path to the template load paths
+      g.templates.unshift File.expand_path('../../templates', __FILE__)
+      # Disable the generation of controller specific assets and helper
+      g.assets false
+      g.helper false
+    end
+
     # We remove the fieldWithErrors div tag that Rails wraps around form elements.
     # It is not used by NdrUi::BootstrapBuilder.
     config.action_view.field_error_proc = proc { |html, _instance| html }
