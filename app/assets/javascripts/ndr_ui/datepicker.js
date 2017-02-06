5671f6499645ce53272d1466c623f4af4c1a2f74
Tim Gentry <tpgentry@me.com>
2017-02-06 20:12:16 +0000

    Revert "# append inline errors and warnings help block after `input group`"
    This reverts commit abae1bb242ba03746e7e362b89b1335288054894.

--- app/assets/javascripts/ndr_ui/datepicker.js
+++ app/assets/javascripts/ndr_ui/datepicker.js
@@ -1,8 +1,1 @@
 //= require bootstrap-datepicker-1.6.0-dist/js/bootstrap-datepicker
-
-jQuery(document).ready(function(){
-  // append inline warning block after `.input-group` block
-  jQuery('.input-group[data-provide="datepicker"]').each(function(){
-    jQuery(this).find(".help-block").insertAfter(jQuery(this));
-  });
-});
