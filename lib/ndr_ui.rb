c4191098f1ff7f0a5441839fef5ab9a04441b8db
Josh Pencheon <josh.pencheon@googlemail.com>
2018-12-14 10:27:47 +0000

    # style: don't copy-and-paste from Rails!

--- lib/ndr_ui.rb
+++ lib/ndr_ui.rb
@@ -6,5 +6,5 @@ module NdrUi
 end
 
 ActiveSupport.on_load(:i18n) do
-  I18n.load_path << File.expand_path("ndr_ui/locale/en.yml", __dir__)
+  I18n.load_path << File.expand_path('ndr_ui/locale/en.yml', __dir__)
 end
